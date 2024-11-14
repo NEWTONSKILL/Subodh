dea74e2f074d5b1d5074fceb1781362c09a1b5b1
SUBODH KUMAR <155408179+NEWTONSKILL@users.noreply.github.com>
2024-11-14 16:38:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Toggle navigation menu on mobile
+Document.addEventListen('DOMContentLoaded' , () => {
 const toggleButton = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
